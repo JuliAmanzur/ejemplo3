97851afe0151c2a58ee34726cc07374c70954c2d
rgquinteros <rgqspain@gmail.com>
2023-08-10 10:45:13 -0300

    trabajando

---
+++ src/ejemplo3/gustavo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo3;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class gustavo {
+/* trabajando*/    
+}
